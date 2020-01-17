038a67ef7338b2b91dc42a45082df5def5577c3c
kevin <490516343@qq.com>
2020-01-18 00:24:51 +0800

    1111

--- src/com/kevin/github/GitHubCommit.java
+++ src/com/kevin/github/GitHubCommit.java
@@ -5,5 +5,8 @@ public class GitHubCommit {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello GitHub!");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("修改代码！");
+		}
 	}
 }
